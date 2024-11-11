353c07b7fc7598b6424e42b688c269b2ca2df988
AurelioFesta04 <auryfesta@gmail.com>
2024-11-11 20:49:19 +0100

    feat: non riesco a capire bene l'esercizio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
-// imposto le variabili
 const timerElem = document.getElementById("timer")
 const formElem = document.getElementById("user-form")
-const listaRandom = document.querySelectorAll(".lista-random li")
-const listaUser = document.querySelectorAll(".lista-user li")
+let listaRandom = document.querySelectorAll(".lista-random .row-col .d-flex .justify-content-between .align-items-center .p-4 li")
+let listaUser = document.querySelectorAll(".lista-user .row-col .d-flex .justify-content-between .align-items-center .p-1 li")
 const confirmBtn = document.getElementById("conferma")
 const finalResult = document.getElementById("result")
 
-// creo un countdown
+
+let randomNum = []
+
+for (i = 0; i < randomNum[i]; i++) {
+    randomNum[i]= Math.floor(Math.random() *50) + 1;
+}
 
 let timer = 5;
 
@@ -20,5 +24,17 @@ const countDown = setInterval(function () {
         timerElem.innerHTML = "TEMPO ESAURITO!!!";
         clearInterval(countDown);
     }
+
+    
+
+}, 1000);
+
+const switchList = setInterval(function () {
+
+    if (timerElem.innerHTML !== "TEMPO ESAURITO!!!") {
+        listaRandom = parseInt(randomNum);
+    } else if (timerElem.innerHTML === "TEMPO ESAURITO!!!") {
+    }
+
 }, 1000);
 
