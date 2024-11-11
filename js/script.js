a766175dff06e49911daa0722e95a470edfefd68
AurelioFesta04 <auryfesta@gmail.com>
2024-11-11 20:15:14 +0100

    refactoring:move in the trash a lot of elements

--- js/script.js
+++ js/script.js
@@ -1,64 +1,24 @@
+// imposto le variabili
 const timerElem = document.getElementById("timer")
-
 const formElem = document.getElementById("user-form")
-
-let randomElem = document.querySelector(".random")
-let userNumElem = document.querySelector(".user-num")
-
+const listaRandom = document.querySelectorAll(".lista-random li")
+const listaUser = document.querySelectorAll(".lista-user li")
 const confirmBtn = document.getElementById("conferma")
 const finalResult = document.getElementById("result")
 
-let numbers = [0, 0, 0, 0, 0];
+// creo un countdown
 
-for (let i = 0; i < 5; i++) {
-    const randomNum = Math.floor(Math.random() * 50) + 1;
-    numbers.push(randomElem);
-}
+let timer = 5;
 
-let timer = 3;
+const countDown = setInterval(function () {
+    console.log(timer);
 
-const intervalTime = setInterval(function () {
-    timerElem.innerHTML = timer;
-    timer--;
-
-    if (timer < 0) {
-        timerElem.innerHTML = "TEMPO FINITO";
-        clearInterval(intervalTime);
-    } else if (timerElem.innerHTML === "TEMPO FINITO") {
-        randomElem.add("d-none")
-        clearInterval(intervalTime);
-        intervalTime = null;
-        randomElem.classList.add("d-none");
-        userNumElem.classList.remove("d-none");
+    if (timer > 0) {
+        timerElem.innerHTML = timer;
+        timer--;
+    } else if (timer = -1) {
+        timerElem.innerHTML = "TEMPO ESAURITO!!!";
+        clearInterval(countDown);
     }
 }, 1000);
 
-formElem.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const userNum = document.querySelectorAll("input");
-    for (let i = 0; i < userNum.length; i++) {
-        curItem = userNum[i];
-        if (randomCount.includes(curItem)) {
-            userNum.push(curItem);
-        }
-    }
-
-    
-    finalResult.classList.remove("d-none");
-    finalResult.innerHTML = `Hai indovinato ${userNum.length} numeri (${userNum})`;
-});
-
-
-
-// function generaNumeri() {
-
-//     
-
-//     for (i = 0; i < numbers.length; i++) {
-//         numbers[i] = Math.floor(Math.random() * 50) + 1;
-//         randomElem.innerHTML +=  `<li class="random col mx-5">${numbers}</li>`
-//     }
-//     return numbers;
-    
-// }
