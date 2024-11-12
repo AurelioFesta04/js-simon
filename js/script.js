e49a428e1eee564d979b15ec3d37cf581466196f
AurelioFesta04 <auryfesta@gmail.com>
2024-11-12 19:27:54 +0100

    feat: esercizio copiato da olga

--- js/script.js
+++ js/script.js
@@ -1,31 +1,96 @@
-const timerElem = document.getElementById("timer")
-const formElem = document.getElementById("user-form")
-let listaRandom = document.querySelectorAll(".lista-random li")
-let listaUser = document.querySelectorAll(".lista-user li")
-const confirmBtn = document.getElementById("conferma")
-const finalResult = document.getElementById("result")
+// Funzioni di appoggio
+const getRandomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
 
-let randomNum = [];
+const getRandomNumbersArray = (min, max, length) => {
+    const numsArray = [];
+    for (let i = 0; i < length; i++) {
+        const randomNum = getRandomInt(min, max);
+        numsArray.push(randomNum);
+    }
+    return numsArray;
+};
 
-for (let i = 0; i < 5; i++) {
-    let curRandomNum = Math.floor(Math.random() * 50) + 1;
-    randomNum.push(curRandomNum);
-    listaRandom[i].innerHTML = curRandomNum;
+///////////////////////
+// Elementi html
+//////////////////////
+const startBtn = document.getElementById("start-btn");
+const numbersListElem = document.getElementById("numbers-list");
+const countdownElem = document.getElementById("countdown");
+const formElem = document.getElementById("answers-form");
+const inputsList = document.querySelectorAll("input");
+const messageElem = document.getElementById("message");
+
+///////////////////////
+// Variabili globali
+//////////////////////
+let numbersArray = [];
+
+const clearGame = () => {
+    messageElem.innerHTML = "";
+    formElem.classList.add("d-none");
+    formElem.reset();
+};
+
+const printNumbers = (numbersToShow) => {
+    // Mostro i numeriu random
+    for (let i = 0; i < numbersToShow.length; i++) {
+        const curNumber = numbersToShow[i];
+        numbersListElem.innerHTML += `<li>${curNumber}</li>`;
+    }
+};
+
+const showForm = () => {
+    numbersListElem.innerHTML = "";
+    countdownElem.innerHTML = "";
+    formElem.classList.remove("d-none");
+    startBtn.disabled = false;
 }
 
-let timer = 5;
-
-    const countDown = setInterval(function () {
-        console.log(timer);
-
-        if (timer > 0) {
-            timerElem.innerHTML = timer;
-            timer--;
-        } else if (timer === 0) {
-            timerElem.innerHTML = "TEMPO ESAURITO!!!";
-            clearInterval(countDown);
-            lista1.classlist.add("d-none");
-            lista2.classlist.remove("d-none");
+const startTimer = (start, end) => {
+    let counter = start;
+    const intervalId = setInterval(() => {
+        if (counter >= end) {
+            countdownElem.innerHTML = counter--;
+        } else {
+            clearInterval(intervalId);
+            showForm();
         }
-
     }, 1000);
+};
+
+const startGame = () => {
+    clearGame();
+    startBtn.disabled = true;
+    numbersArray = getRandomNumbersArray(1, 100, 5);
+    console.log(numbersArray);
+    printNumbers(numbersArray);
+    // Faccio partire il timer
+    startTimer(30, 0);
+};
+
+const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit");
+    const insertedNumbers = [];
+    for (let i = 0; i < inputsList.length; i++) {
+        const curInput = inputsList[i];
+        insertedNumbers.push(parseInt(curInput.value));
+    }
+    console.log(insertedNumbers);
+
+    const correctNumbers = [];
+    for (let i = 0; i < insertedNumbers.length; i++) {
+        const curNumber = insertedNumbers[i];
+        if (numbersArray.includes(curNumber)) {
+            correctNumbers.push(curNumber);
+        }
+    }
+    console.log(correctNumbers);
+
+    messageElem.innerHTML = `Hai indovinato ${correctNumbers.length
+        } numeri: ${correctNumbers.join(",")}`;
+};
+
+startBtn.addEventListener("click", startGame);
+formElem.addEventListener("submit", handleFormSubmit);
